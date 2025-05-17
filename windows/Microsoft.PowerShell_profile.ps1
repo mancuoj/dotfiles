fc6f6d538036fb9a757a5b32aee73ddd63305d83
mancuoj <mancuoj@gmail.com>
2025-05-18 02:21:53 +0800

    update

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -4,15 +4,27 @@ $newPath = $currentPath + ";" + $shimPath
 [Environment]::SetEnvironmentVariable('Path', $newPath, 'User')
 
 Remove-Item Alias:ni -Force -ErrorAction Ignore
+Remove-Item Alias:ls -Force -ErrorAction Ignore
 
-$env:HTTP_PROXY="http://127.0.0.1:7897";
-$env:HTTPS_PROXY="http://127.0.0.1:7897"
+Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardDeleteLine
 
+function Invoke-NrStart { nr start }
+function Invoke-NrDev { nr dev }
+function Invoke-NrBuild { nr build }
+function Invoke-Eza { eza --icons=always }
 function up {
   scoop update *
   if ($?) {
     scoop cleanup *
   }
 }
+
+Set-Alias -Name s -Value Invoke-NrStart
+Set-Alias -Name d -Value Invoke-NrDev
+Set-Alias -Name b -Value Invoke-NrBuild
+Set-Alias -Name ls -Value Invoke-Eza
+
+$env:HTTP_PROXY="http://127.0.0.1:7897";
+$env:HTTPS_PROXY="http://127.0.0.1:7897"
 
 Invoke-Expression (&starship init powershell)
